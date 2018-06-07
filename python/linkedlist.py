6e9ae3ad2fe566dc82f03069dcd41d2c32dc1cb8
mjpin7 <mjpin7@gmail.com>
2018-06-07 10:54:19 -0400

    Added rest of file

--- python/linkedlist.py
+++ python/linkedlist.py
@@ -40,6 +40,10 @@ class LinkedList:
     # @returns              The value at that index
     def value_at(self, index):
         temp = self.head
+
+        if index > self.size:
+            print("Invalid index")
+            return
         
         # If the list is empty
         if self.size() == 0:
@@ -59,7 +63,163 @@ class LinkedList:
                 # Return the value at that index
                 return temp.value
 
+    # Function to add a value to the beginning of the list
+    #
+    # @param value          The value to insert
+    def push_front(self, value):
+
+        newNode = Node(value=value)
+        newNode.next = self.head
+
+        self.head = newNode
+
+    # Function to remove and return the first value in the list
+    #
+    # @returns          The value that was at the begininning
+    def pop_front(self):
+        temp = self.head
+
+        # If the list is empty
+        if self.empty():
+            print("No values to pop off")
+            return
+        # If the list is not empty
+        else:
+            val = self.head.value
+            self.head = self.head.next
+
+            return val
+
+    # Function to place a vlaue at the end of the list
+    #
+    # @Param value          The value to insert
+    def push_back(self, value):
+        n = Node(value=value)
+
+        if self.empty():
+            self.head = n
+            return
+        
+        temp = self.head
+        while temp.next is not None:
+            temp = temp.next
+
+        
+        temp.next = n
+ 
+    # Function to remove and return the value at the end of the list
+    #
+    # @Returns          The value removed at the end
+    def pop_back(self):
+        if self.empty():
+            print("No values to pop off")
+            return
+        elif self.size() == 1:
+            val = self.head.value
+            self.head = self.head.next
+            return val
+        else:
+            temp = self.head
+            prev = temp
+
+            while temp.next is not None:
+                prev = temp
+                temp = temp.next
+
+            val = temp.value
+            prev.next = None
+            return val
+
+    # Function to return the value at the beginning without removing it
+    #
+    # @Returns          The value at the beginning
+    def front(self):
+        return self.head.value
+
+    # Function to return the value at the end without removing it
+    #
+    # @Returns          The value at the end
+    def back(self):
+        temp = self.head
+
+        while temp.next is not None:
+            temp = temp.next
+        
+        return temp.value
+
+    # Function to insert a value at a given index
+    #
+    # @Param index          The index to insert it to
+    # @Param value          The value to insert
+    def insert(self, index, value):
+        n = Node(value=value)
+        temp = self.head
+
+        if index > self.size:
+            print("Invalid index")
+            return
+
+        # If wanting to insert to the begininning
+        if index == 0:
+            self.push_front(value)
+            print("Value %d inserted at index %d." % (value, index))
+            return
+
+        # Else, traverse through the list
+        while index > 1:
+            temp = temp.next
+            index -= 1
+        
+        n.next = temp.next
+        temp.next = n
+
+        print("Value %d inserted at index %d." % (value, index))
+
+    # Function to delete a node at a given index
+    #
+    # @Param index          The node to delete
+    #
+    # @Returns              The value at the deleted node
+    def erase(self, index):
+        temp = self.head
+        prev = temp
+
+        if index > self.size:
+            print("Invalid index")
+            return
+
+        # If wanting to erase the first value, call pop_front function
+        if index == 0:
+            return self.pop_front()
+        
+        # Else, traverse through the list
+        while index > 0:
+            prev = temp
+            temp = temp.next
+            index -= 1
+        
+        val = temp.value
+        prev.next = temp.next
+        temp = None
+        return val
+
 
 list = LinkedList()
-print(list.empty())
+print("Pushed 56 to front")
+list.push_front(56)
+print("Pushed 34 to front")
+list.push_front(34)
+print("Popped back %d" % (list.pop_back()))
+print("Popped back %d" % (list.pop_back()))
+print("Is empty", list.empty())
+list.pop_back()
+list.insert(0, 89)
+list.insert(1, 9)
+print("Front %d" % (list.front()))
+print("Back %d" % (list.back()))
+print("Erased index 1: %d" % (list.erase(1)))
+print("Size: %d" % (list.size()))
+print("Erased index 0: %d" % (list.erase(0)))
+print("Size: %d" % (list.size()))
+
 
